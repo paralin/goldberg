e25c51581a8b3a74c38b0928357219d65de448cd
Detanup01 <91248446+Detanup01@users.noreply.github.com>
2025-11-23 18:38:04 +0100

    return null instead of actual interface get

--- dll/dll.cpp
+++ dll/dll.cpp
@@ -325,11 +325,12 @@ static void *create_client_interface(const char *ver)
             return static_cast<ISteamClient *>(client_ptr);
         }
 
-        client_ptr->report_missing_impl_and_exit(ver, EMU_FUNC_NAME);
+        // report the missing interface if it is a client
+        client_ptr->report_missing_impl(ver, EMU_FUNC_NAME);
     }
     
-    // Assuming old games (1110100) and old version of Facepunch.Steamworks call this first then it will throw because this is not here.
-    return get_steam_client()->GetISteamGenericInterface(SteamAPI_GetHSteamUser(), SteamAPI_GetHSteamPipe(), ver);
+    // Original steam returns 0 here.
+    return nullptr;
 }
 
 STEAMAPI_API void * S_CALLTYPE SteamInternal_CreateInterface( const char *ver )
